0dc65242e30269d348303ceb7f2a6958da636ea0
coder123n <naomisellis@gmail.com>
2019-09-03 10:03:16 -0700

    js

---
+++ myscript.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+	$("#hide").click(function(){
+		$("#hide").hide();
+	});
+	$("img").click(function(){
+		$("p").show();
+		$("h2").show();
+	});
+	$(".stl").css({
+	"color":"red"}
+		
+	});
+});
